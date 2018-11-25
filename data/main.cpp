9b652593b278e2719ee6fb2073a60b6d73970929
Piotr Henczel <henczel.piotr@gmail.com>
2018-11-25 22:30:30 +0100

    Rozwiazania do zadan z egzaminu

--- data/main.cpp
+++ data/main.cpp
@@ -11,23 +11,23 @@ class data{
 		friend std::ostream& operator<<(std::ostream&, data&);
 };
 
-	data::data(){
-		dzien = 0;
-		miesiac = 0;
-		rok = 0;
-	}
+data::data(){
+	dzien = 0;
+	miesiac = 0;
+	rok = 0;
+}
 
-	std::istream& operator>>(std::istream& wejscie, data& obiekt){
-		wejscie>>obiekt.dzien;
-		wejscie>>obiekt.miesiac;
-		wejscie>>obiekt.rok;
-		return wejscie;
-	}
+std::istream& operator>>(std::istream& wejscie, data& obiekt){
+	wejscie>>obiekt.dzien;
+	wejscie>>obiekt.miesiac;
+	wejscie>>obiekt.rok;
+	return wejscie;
+}
 	
-	std::ostream& operator<<(std::ostream& wyjscie, data& obiekt){
-		wyjscie<<"Podano date: "<<obiekt.dzien<<"."<<obiekt.miesiac<<"."<<obiekt.rok<<std::endl;
-		return wyjscie;
-	}
+std::ostream& operator<<(std::ostream& wyjscie, data& obiekt){
+	wyjscie<<"Podano date: "<<obiekt.dzien<<"."<<obiekt.miesiac<<"."<<obiekt.rok<<std::endl;
+	return wyjscie;
+}
 
 int main(int argc, char** argv) {
 	data obiekt1;
